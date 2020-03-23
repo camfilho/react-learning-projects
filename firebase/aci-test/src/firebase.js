8496197bcdb00d526c549ed610e2452c2ecb3f33
camfilho <camfilho@ufrn.edu.br>
2020-03-23 12:45:25 -0300

    set up firebase

---
+++ firebase/aci-test/src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: 
+    authDomain:
+    databaseURL:
+    projectId:
+    storageBucket:
+    messagingSenderId:
+    appId: 
+  };
+  // Initialize Firebase
+  firebase.initializeApp(config);
+
+  export default firebase;
